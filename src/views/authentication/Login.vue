b1df238b0f8fa64c37111a4d731342d4bf1f2b64
JC Bombardelli <jc.bombardelli@live.com>
2021-04-12 23:25:11 -0300

    chore: login form

--- src/views/authentication/Login.vue
+++ src/views/authentication/Login.vue
@@ -1,14 +1,49 @@
 <template>
-  <div>
-  </div>
+  <form @submit.prevent="onSubmit">
+    <div class="login-page">
+      <div class="card">
+        <div class="card-header">Login</div>
+        <div class="card-body">
+          <div class="form-group">
+            <input type="email" v-model="form.username" class="form-control" placeholder="E-mail" />
+          </div>
+          <div class="form-group">
+            <input type="password" v-model="form.password" class="form-control" placeholder="Password"  />
+          </div>
+          <button class="btn btn-primary w-100">Login</button>
+        </div>
+      </div>
+    </div>
+  </form>
 </template>
 
 <script>
-export default {
+import Vue from 'vue'
 
-}
+export default Vue.extend({
+  name: 'Login',
+  data: () => ({
+    form: {
+      username: '',
+      password: ''
+    }
+  }),
+  methods: {
+    onSubmit () {
+      console.log(this.form)
+    }
+  }
+})
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+.login-page {
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.card {
+  width: 30%;
+}
 </style>
