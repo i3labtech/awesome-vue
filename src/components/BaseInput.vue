a486e7e60f8a21d78822865e2c2fe4a74ced8e22
JC Bombardelli <jc.bombardelli@live.com>
2021-04-23 21:19:08 -0300

    chore: input base

---
+++ src/components/BaseInput.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="form-group">
+    <label :for="id">{{label}}</label>
+    <input class="form-control" :value="value"
+      :type="type" :id="id" :placeholder="placeholder ? placeholder : label"
+    />
+    <small v-if="helper" class="form-text text-muted">  {{helper}} </small>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'BaseInput',
+  props: {
+    required: Boolean,
+    id: String,
+    label: String,
+    type: String,
+    helper: String,
+    placeholder: String,
+    value: String
+  }
+})
+</script>
+
+<style></style>
